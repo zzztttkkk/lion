1614c686be2d334673eb475266e9980b24f5d510
zzztttkkk <ztkisalreadytaken@gmail.com>
2024-12-15 15:00:52 +0800

    [🧹 Chore] (/): -

--- get_set_test.go
+++ get_set_test.go
@@ -10,31 +10,46 @@ type A struct {
 	A1 string `json:"a1"`
 }
 
-func noop(v any) {}
+func noop(v any) {
+	_ = v.(*string)
+}
+
+var a1offset int64
+var a1Field reflect.StructField
+
+func init() {
+	a1Field, _ = reflect.TypeOf(A{}).FieldByName("A1")
+
+	var ptr = &A{}
+	a1offset = int64(uintptr(unsafe.Pointer(&ptr.A1))) - int64(uintptr(unsafe.Pointer(ptr)))
+}
 
 func BenchmarkNormalGetFieldPtrByIndex(b *testing.B) {
-	f, _ := reflect.TypeOf(A{}).FieldByName("A1")
-
 	val := A{}
 
 	vv := reflect.ValueOf(&val).Elem()
 	for i := 0; i < b.N; i++ {
-		fv := vv.FieldByIndex(f.Index).Addr().Interface()
+		fv := vv.FieldByIndex(a1Field.Index).Addr().Interface()
 		noop(fv)
 	}
 }
 
 func BenchmarkGetFieldPtrByOffset(b *testing.B) {
-	var ptr = &A{}
-	a1offset := int64(uintptr(unsafe.Pointer(&ptr.A1))) - int64(uintptr(unsafe.Pointer(ptr)))
-
-	f, _ := reflect.TypeOf(A{}).FieldByName("A1")
-
 	val := A{}
 	valptr := unsafe.Pointer(&val)
 
 	for i := 0; i < b.N; i++ {
-		fv := reflect.NewAt(f.Type, unsafe.Add(valptr, a1offset)).Interface()
+		fv := reflect.NewAt(a1Field.Type, unsafe.Add(valptr, a1offset)).Interface()
+		noop(fv)
+	}
+}
+
+func BenchmarkGetFieldPtrByOffsetAndTypecast(b *testing.B) {
+	val := A{}
+	valptr := unsafe.Pointer(&val)
+
+	for i := 0; i < b.N; i++ {
+		fv := (*string)(unsafe.Add(valptr, a1offset))
 		noop(fv)
 	}
 }
